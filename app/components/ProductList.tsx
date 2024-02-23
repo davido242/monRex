ff5178b975091c43e26afb6a57289390650c66a5
Monday_David_Sarka <davidsarka242@gmail.com>
2024-02-23 01:27:37 +0100

    Toggle product iamges on hover with js

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -10,6 +10,15 @@ import { Products }  from "./dummyProducts";
 export default function Product() {
   const router = useRouter();
 
+  const [isHovered, setIsHovered] = useState(null);
+  const handleMouseOver = (productId: number) => {
+    setIsHovered(productId);
+  }
+
+  const handleMouseOut = () => {
+    setIsHovered(null);
+  }
+
   return (
     <div className="pt-2">      
       <div className="p-4 rounded mx-auto">
@@ -17,14 +26,15 @@ export default function Product() {
           {Products.map((product) => (
             <div onClick={() => router.push(`/product-detail/${product.slug}`)} className="group cursor-pointer">
               <div key={product.id} className="p-5">
-                <div className="relative">
+                <div className="relative" onMouseOver={() => handleMouseOver(product.id)}
+                  onMouseOut={handleMouseOut}>
                   <div className="ratings absolute flex w-full top-0 left-0 p-4 justify-between">
                     <div className="uppercase bg-blue-900 flex items-center justify-center w-[40px] h-[25px] rounded text-xs text-white">New</div>
                     <div>
                     <Image src="/assets/images/love.svg" alt="fav icon" width={20} height={25} className="" />
                     </div>
                   </div>
-                  <Image src={product.image.front} alt={product.name} className="rounded-2xl h-auto" />
+                  <Image src={isHovered === product.id ? product.image.front : product.image.back} alt={product.name} className="rounded-2xl h-auto" />
                   <div className="hidden absolute group-hover:block duration-300 bottom-2 left-0 p-2 w-full rounded-lg">
                     <Link href="#" className="bg-white text-center w-full inline-block py-4 rounded-full">Quick View</Link>
                   </div>
